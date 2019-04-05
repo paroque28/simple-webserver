1cc77d345a5bd61c69bd05421cd19639c6d7ede7
espqui <a.espqui@gmail.com>
2019-04-05 12:02:12 -0600

    Update bclient.c

--- Program/Benchmark/bclient.c
+++ Program/Benchmark/bclient.c
@@ -1,76 +1,56 @@
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
-#include <curl/curl.h>
-#include <curl/easy.h>
-#include <stdlib.h> 
+#include <stdio.h> //Used to write the beanchmark
+#include <stdlib.h> //System functions
+#include <string.h> //Used for parse args inserted by the user
+#include <pthread.h> //Used for creating threads.
 #include <unistd.h>  //Header file for sleep(). man 3 sleep for details. 
-#include <pthread.h> 
-#include <time.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <errno.h>
-#include <arpa/inet.h>
-
-#include <string.h>
+#include <time.h> //Used to measure the response and total time.
+#include <netdb.h> //Used to communicate with the server.
 
 
+/*Constants*/
+#define benchmarkName "BenchMark.csv"
+#define  bufferSize 1024
 
+//The client send a querry to the server
 int ReadHttpStatus(int sock){
     char c;
-     
-    char buff[1024]="",*ptr=buff+1;
+    char buff[bufferSize]="",*ptr=buff+1;
     int bytes_received, status;
-    printf("Begin Response ..\n");
     while(bytes_received = recv(sock, ptr, 1, 0)){
         if(bytes_received==-1){
             perror("ReadHttpStatus");
             exit(1);
         }
-
         if((ptr[-1]=='\r')  && (*ptr=='\n' )) break;
         ptr++;
     }
     *ptr=0;
     ptr=buff+1;
-
     sscanf(ptr,"%*s %d ", &status);
-
-    //printf("%s\n",ptr);
-    printf("status=%d\n",status);
-    //printf("End Response ..\n");
     return (bytes_received>0)?status:0;
 
 }
 
-//the only filed that it parsed is 'Content-Length' 
+/*
+ *This function deal with the size of the file
+ *Stop as soon as all the bytes are transferred. 
+ */
 int ParseHeader(int sock){
     char c;
-    char buff[1024]="",*ptr=buff+4;
+    char buff[bufferSize]="",*ptr=buff+4;
     int bytes_received, status;
-    //printf("Begin HEADER ..\n");
     while(bytes_received = recv(sock, ptr, 1, 0)){
         if(bytes_received==-1){
             perror("Parse Header");
             exit(1);
         }
-
-        if(
-            (ptr[-3]=='\r')  && (ptr[-2]=='\n' ) &&
-            (ptr[-1]=='\r')  && (*ptr=='\n' )
-        ) break;
+        if((ptr[-3]=='\r')  && (ptr[-2]=='\n' ) && (ptr[-1]=='\r')  && (*ptr=='\n' ))
+			break;
         ptr++;
     }
 
     *ptr=0;
     ptr=buff+4;
-    //printf("%s",ptr);
-
     if(bytes_received){
         ptr=strstr(ptr,"Content-Length:");
         if(ptr){
@@ -79,107 +59,107 @@ int ParseHeader(int sock){
         }else
             bytes_received=-1; //unknown size
 
-       //printf("Content-Length: %d\n",bytes_received);
     }
-   // printf("End HEADER ..\n");
     return  bytes_received ;
 
 }
 
+/*Handle the most important info of each execution
+* It is helpful to notice what thread is running anywhere in the code*/
+
 typedef struct thread_arg {
-	long int id;
-	int  nthread;
-	int  ncycle;
-	char domain[30];
-	char path[30];
+	int  nthread; //Thread number
+	int  ncycle; //Cycle
+	char domain[30]; //Main address
+	char path[30]; //File name
+	
 }thread_arg;
 
 
 
-
-
-
-
-void* downloadFile(thread_arg * arg){
+/*Make the query and download the specified file, 
+ * It also measures the response time of each cycle of each thread*/
+void* downloadFile(void *data){
+	
+	struct thread_arg *arg = data;
 	
 	char domain[30];
 	char path[30];
 
+
 	strcpy(domain, arg->domain);
 	strcpy(path,arg->path);
+	
+	/*Iterates the cycles*/
+	for (int j=1;j<=arg->ncycle;j++){
+		char thID[30];
+		sprintf(thID, "T%d-C%d",arg->nthread, j);
+		int sock, bytes_received;  
+		char send_data[bufferSize],recv_data[bufferSize], *p;
+		struct sockaddr_in server_addr;
+		struct hostent *he;
+		he = gethostbyname(domain);
+		if (he == NULL){
+		   herror("gethostbyname");
+		   exit(1);
+		}
+		if ((sock = socket(AF_INET, SOCK_STREAM, 0))== -1){
+		   perror("Socket");
+		   exit(1);
+		}
+		server_addr.sin_family = AF_INET;     
+		server_addr.sin_port = htons(80);
+		server_addr.sin_addr = *((struct in_addr *)he->h_addr);
+		bzero(&(server_addr.sin_zero),8); 
+		
+		if (connect(sock, (struct sockaddr *)&server_addr,sizeof(struct sockaddr)) == -1){
+		   perror("Connect");
+		   exit(1); 
+		}
+		snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, domain);
+		if(send(sock, send_data, strlen(send_data), 0)==-1){
+			perror("send");
+			exit(2); 
+		}
+		int contentlengh;
+		clock_t t; 
+		t = clock(); 
+		int auxBytes=0;
+		if(ReadHttpStatus(sock) && (contentlengh=ParseHeader(sock))){
+			t = clock() - t; 
+			double time_taken = ((double)t)/CLOCKS_PER_SEC; // in second
+			FILE* fb=fopen(benchmarkName,"a");//	
+			fprintf(fb, "Response time %s,  %f ,\n",thID, time_taken);//Response time of each cycle of each thread.
+			fclose(fb);
+		int bytes=0;
+		//strcat(thID,path); //Unmark this line if you want to save the file in your computer. Line 1/4
+		//FILE* fd=fopen(thID,"wb"); //Unmark this line if you want to save the file in your computer. Line 2/4
 
-    int sock, bytes_received;  
-    char send_data[1024],recv_data[1024], *p;
-    struct sockaddr_in server_addr;
-    struct hostent *he;
+		while(bytes_received = recv(sock, recv_data, bufferSize, 0)){
+			if(bytes_received==-1){
+				perror("recieve");
+				exit(3);
+			}
+			//fwrite(recv_data,1,bytes_received,fd);//Unmark this line if you want to save the file in your computer. Line 3/4
+			bytes+=bytes_received;
+			auxBytes+=bytes_received;
 
+			if(bytes==contentlengh)	
+				break;
 
-    he = gethostbyname(domain);
-    if (he == NULL){
-       herror("gethostbyname");
-       exit(1);
-    }
+		}
+			//fclose(fd);//Unmark this line if you want to save the file in your computer. Line 4/4
+		}
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0))== -1){
-       perror("Socket");
-       exit(1);
-    }
-    server_addr.sin_family = AF_INET;     
-    server_addr.sin_port = htons(80);
-    server_addr.sin_addr = *((struct in_addr *)he->h_addr);
-    bzero(&(server_addr.sin_zero),8); 
-
-    printf("Connecting ...\n");
-    if (connect(sock, (struct sockaddr *)&server_addr,sizeof(struct sockaddr)) == -1){
-       perror("Connect");
-       exit(1); 
-    }
-
-
-    snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, domain);
-
-    if(send(sock, send_data, strlen(send_data), 0)==-1){
-        perror("send");
-        exit(2); 
-    }
-    //printf("Data sent.\n");  
-
-    //fp=fopen("received_file","wb");
-    //printf("Recieving data...\n\n");
-
-    int contentlengh;
-	time_t beginR; //Total Execution time
-	time_t endR;
-	time(&beginR);
-    
-    if(ReadHttpStatus(sock) && (contentlengh=ParseHeader(sock))){
-
-	 time(&endR);
-	 printf("Response time %f\n", difftime(endR, beginR));
-	int bytes=0;
-        
-        FILE* fd=fopen(path,"wb");
-        printf("Saving data...");
-
-        while(bytes_received = recv(sock, recv_data, 1024, 0)){
-            if(bytes_received==-1){
-                perror("recieve");
-                exit(3);
-            }
-            fwrite(recv_data,1,bytes_received,fd);
-            bytes+=bytes_received;
-            //printf("Bytes recieved: %d\n",bytes);
-            if(bytes==contentlengh)
-            break;
-        }
-        fclose(fd);
-    }
-
-
-
-    close(sock);
-    printf("\n\nDone.\n\n");
-
+		close(sock);
+		//It measures the bytes received
+		if(j==1){
+			FILE* fb=fopen("BenchMark.csv","a");				
+			fprintf(fb, "Bytes received T%d:, %d, \n",arg->nthread, auxBytes);
+			fclose(fb);
+		}
+		
+	}
 }
 
 
@@ -189,9 +169,10 @@ void* downloadFile(thread_arg * arg){
 
 int main(int argc, char *argv[]){
  
-  // # bclient <machine> <port> <file> <N-threads> <N-cycles>
-	 time_t begin; //Total Execution time
-	 time_t end;
+	// # bclient <machine> <port> <file> <N-threads> <N-cycles>
+	
+	clock_t t; //Total Execution time
+   
      
 	if( argc ==6){
 		char machine[20];
@@ -200,57 +181,55 @@ int main(int argc, char *argv[]){
 		int threads=0;
 		int cycles=0;
 		
-		 strcpy(machine, argv[1]);
-		 strcpy(file, argv[3]);
-			
-		
+		/*Parsing arguments*/
+		strcpy(machine, argv[1]);
+		strcpy(file, argv[3]);
 		threads=atoi(argv[4]);
 		port=atoi(argv[2]);
 		cycles=atoi(argv[5]);
 		
 		
+		FILE* fb=fopen("BenchMark.csv","wb");
+		//fwrite(recv_data,1,bytes_received,fb);
 		
-		printf("Machine: %s \n", machine); // 
-		printf("file: %s \n", file); // 
-		printf("Port: %d \n", port); // 
-		printf("Threads:  %d \n", threads); // 
-		printf("Cycles: %d \n", cycles); // 
-		//downloadFile();
-		printf("\nURL: %s/%s\n", machine,file);
+		fprintf(fb,"Machine, %s, \n", machine); // 
+		fprintf(fb,"file, %s, \n", file); // 
+		fprintf(fb,"Port, %d, \n", port); // 
+		fprintf(fb,"Threads,  %d, \n", threads); // 
+		fprintf(fb,"Cycles, %d, \n", cycles); // 
+		fprintf(fb,"URL, %s/%s,\n", machine,file);
+		fclose (fb);
+		pthread_t thread_id[threads]; 
+		struct thread_arg arg[threads];
 		
-
-	 pthread_t thread_id[threads]; 
-	  struct thread_arg arg[threads];
-	  
-	  
-    time(&begin);
-    time_t test = clock();
+		 t = clock(); 
     
-	  for(int i=0;i<threads;i++)
-    {
-		 arg[i].nthread = i+1;
-		 arg[i].ncycle=cycles;
-		 strcpy(arg[i].domain, machine);
-		  strcpy(arg[i].path,file);
-		pthread_create(&thread_id[i], NULL, downloadFile, &arg[i]); 
-		 arg[i].id = thread_id[i];
-		// printf("In main \nthread id = %ld\n", (long)  arg[i].id );
-		//printf("In main nthread id = %d\n",  arg.nthread );
+		for(int i=0;i<threads;i++){
+			arg[i].nthread = i+1;
+			arg[i].ncycle=cycles;
+
+			strcpy(arg[i].domain, machine);
+			strcpy(arg[i].path,file);
+			pthread_create(&thread_id[i], NULL, &downloadFile, &arg[i]); 
+
+			//arg[i].id = thread_id[i];
+			// printf("In main \nthread id = %ld\n", (long)  arg[i].id );
+			//printf("In main nthread id = %d\n",  arg.nthread );
+
+		} 
 		
-	} 
-	
-	 for(int i=0;i<threads;i++)
-    {
-		pthread_join(thread_id[i], NULL); 
-		
-	}
-	   
-   time(&end);
-	printf("Total execution time %f\n", difftime(end, begin));
-	pthread_exit(NULL); 
-		
-	}
+		for(int i=0;i<threads;i++){
+			pthread_join(thread_id[i], NULL); 	
+		}
+		   
+		t = clock() - t; 
+		double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+		fb=fopen("BenchMark.csv","a");	
+		fprintf(fb, "Total execution time, %f\n", time_taken);
+		fclose(fb);
+	  
+		pthread_exit(NULL); 	
+		}
 	return 0;
 }
-
 
